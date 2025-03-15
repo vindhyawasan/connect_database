d428d17e60613f29d169b2aae32e0676ff8c3a57
vindhyawasan <asingh128ve@gmail.com>
2025-03-15 16:26:58 +0530

    here show all code to connect with data base

--- mysql_conn.py
+++ mysql_conn.py
@@ -1,1 +1,29 @@
 import mysql.connector
+# Connect to the database
+mydb = mysql.connector.connect(host="Localhost",username="root",password="opencv@re",database="college")
+print(mydb)
+mycursor = mydb.cursor()
+# mycursor.execute("DROP TABLE IF EXISTS CUSTOMER")
+# mycursor.execute("""
+#                  CREATE TABLE CUSTOMER(
+#                      NAME VARCHAR(30),
+#                      AGE INT,
+#                      EMAIL VARCHAR(50)PRIMARY KEY)""")
+# mycursor.execute("USE COLLEGE")
+mycursor.execute("""INSERT INTO CUSTOMER(NAME,AGE,EMAIL)
+    VALUES
+    ("VINDHYAWASAN",20,"ASINGH128VE@GMAIL.COM"),
+    ("SHUBHAM", 16,"SHUBHAM128VE@GMAIL.COM")""")
+
+# mycursor.execute("""
+#     INSERT INTO CUSTOMER (NAME, AGE, EMAIL)
+#     VALUES
+#     ("VINDHYAWASAN", 20, "ASINGH128VE@GMAIL.COM"),
+#     ("SHUBHAM", 16, "SHUBHAM128VE@GMAIL.COM")
+# """)
+mydb.commit()
+print(mycursor.rowcount,"recrod insert")
+
+for x in mycursor:
+    print(x)
+    # pass
